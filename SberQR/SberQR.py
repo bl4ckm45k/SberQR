49c342c583bf7a30ea8c63f6f530411fe2d36078
bl4ckm45k <nonpowa@gmail.com>
2022-09-05 16:25:08 +0300

    edit creation, moved Types and Scope, added RegistryType usage

--- SberQR/SberQR.py
+++ SberQR/SberQR.py
@@ -3,25 +3,13 @@ from datetime import datetime
 from logging import getLogger
 from random import choices
 from string import hexdigits
+from typing import Union, List, Dict
 
 from requests_pkcs12 import post
-
-from enum import Enum
+from .scope import Scope
+from .types import CancelType, RegistryType
 
 logger = getLogger(__name__)
-
-
-class Scope(Enum):
-    create = 'https://api.sberbank.ru/qr/order.create'
-    status = 'https://api.sberbank.ru/qr/order.status'
-    revoke = 'https://api.sberbank.ru/qr/order.revoke'
-    cancel = 'https://api.sberbank.ru/qr/order.cancel'
-    registry = 'auth://qr/order.registry'
-
-
-class CancelType(Enum):
-    REFUND = 'REFUND'
-    REVERSE = 'REVERSE'
 
 
 class SyncSberQR(object):
@@ -38,30 +26,31 @@ class SyncSberQR(object):
             member_id: str, id_qr: str, tid: str, client_id: str, client_secret: str,
             pkcs12_filename: str, pkcs12_password: str,
     ):
-        self.member_id = member_id
-        self.id_qr = id_qr
-        self.tid = tid
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.pkcs12_filename = pkcs12_filename
-        self.pkcs12_password = pkcs12_password
+        self._member_id = member_id
+        self._id_qr = id_qr
+        self._tid = tid
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._pkcs12_filename = pkcs12_filename
+        self._pkcs12_password = pkcs12_password
+        self._sbp_member_id = "100000000111"
 
     def token(self, scope: Scope):
         url = 'https://api.sberbank.ru:8443/prod/tokens/v2/oauth'
-        auth = base64.b64encode(f'{self.client_id}:{self.client_secret}'.encode('utf-8')).decode('utf-8')
+        auth = base64.b64encode(f'{self._client_id}:{self._client_secret}'.encode('utf-8')).decode('utf-8')
         headers = {
             'Accept': 'application/json',
             'Authorization': f'Basic {auth}',
             'Content-Type': 'application/x-www-form-urlencoded',
             'rquid': ''.join(choices(hexdigits, k=32)),
-            'x-ibm-client-id': self.client_id,
+            'x-ibm-client-id': self._client_id,
         }
         response = post(
             url,
             data={'grant_type': 'client_credentials', 'scope': scope.value},
             headers=headers,
-            pkcs12_filename=self.pkcs12_filename,
-            pkcs12_password=self.pkcs12_password
+            pkcs12_filename=self._pkcs12_filename,
+            pkcs12_password=self._pkcs12_password
         )
 
         logger.info(response.status_code)
@@ -70,7 +59,7 @@ class SyncSberQR(object):
 
         return result['access_token']
 
-    def creation(self, name: str, position_sum: int, is_sbp=True):
+    def creation(self, description: str, order_sum: int, order_number: str, positions: Union[List, Dict]):
         """
         Создает новый динамический QR код
         """
@@ -84,37 +73,32 @@ class SyncSberQR(object):
             'Accept': 'application/json',
             'Authorization': f'Bearer {access_token}',
             'RqUID': rq_uid,
-            'x-ibm-client-id': self.client_id,
+            'x-ibm-client-id': self._client_id,
         }
+        if isinstance(positions, dict):
+            positions = [positions]
         data = {
             "rq_uid": rq_uid,
             "rq_tm": dt,
-            "member_id": self.member_id,
-            "order_number": now.strftime("%Y%m%d%H%M%S%f"),
+            "member_id": self._member_id,
+            "order_number": order_number,
             "order_create_date": dt,
-            "order_params_type": [
-                {
-                    "position_name": name,
-                    "position_count": 1,
-                    "position_sum": position_sum,
-                    "position_description": name
-                }
-            ],
-            "id_qr": self.id_qr,
-            "order_sum": position_sum,
+            "order_params_type": positions,
+            "id_qr": self._id_qr,
+            "order_sum": order_sum,
             "currency": "643",
-            "description": name,
+            "description": description,
         }
+        is_sbp = True if self._tid == self._id_qr else False
         if is_sbp:
-            # data['sbp_member_id'] = "110000000111"  # Из доки
-            data['sbp_member_id'] = "100000000111"  # из письма поддержки
+            data['sbp_member_id'] = self._sbp_member_id
 
         response = post(
             url,
             json=data,
             headers=headers,
-            pkcs12_filename=self.pkcs12_filename,
-            pkcs12_password=self.pkcs12_password
+            pkcs12_filename=self._pkcs12_filename,
+            pkcs12_password=self._pkcs12_password
         )
         logger.info(response.status_code)
         logger.info(response.text)
@@ -130,7 +114,7 @@ class SyncSberQR(object):
             "rq_uid": rq_uid,
             "rq_tm": f'{datetime.utcnow().isoformat(timespec="seconds")}Z',
             "order_id": order_id,
-            "tid": self.tid,
+            "tid": self._tid,
             "partner_order_number": partner_order_number
         }
 
@@ -140,11 +124,11 @@ class SyncSberQR(object):
                 'Accept': 'application/json',
                 'Authorization': f'Bearer {access_token}',
                 'RqUID': rq_uid,
-                'x-ibm-client-id': self.client_id,
+                'x-ibm-client-id': self._client_id,
             },
             json=data,
-            pkcs12_filename=self.pkcs12_filename,
-            pkcs12_password=self.pkcs12_password
+            pkcs12_filename=self._pkcs12_filename,
+            pkcs12_password=self._pkcs12_password
         )
         logger.info(response.status_code)
         logger.info(response.text)
@@ -162,15 +146,15 @@ class SyncSberQR(object):
                 'Accept': 'application/json',
                 'Authorization': f'Bearer {access_token}',
                 'RqUID': rq_uid,
-                'x-ibm-client-id': self.client_id,
+                'x-ibm-client-id': self._client_id,
             },
             json={
                 "rq_uid": rq_uid,
                 "rq_tm": f'{datetime.utcnow().isoformat(timespec="seconds")}Z',
                 "order_id": order_id,
             },
-            pkcs12_filename=self.pkcs12_filename,
-            pkcs12_password=self.pkcs12_password
+            pkcs12_filename=self._pkcs12_filename,
+            pkcs12_password=self._pkcs12_password
         )
         logger.info(response.status_code)
         logger.info(response.text)
@@ -193,29 +177,29 @@ class SyncSberQR(object):
                 'Accept': 'application/json',
                 'Authorization': f'Bearer {access_token}',
                 'RqUID': rq_uid,
-                'x-ibm-client-id': self.client_id,
+                'x-ibm-client-id': self._client_id,
             },
             json={
                 "rq_uid": rq_uid,
                 "rq_tm": f'{datetime.utcnow().isoformat(timespec="seconds")}Z',
                 "operation_id": operation_id,
                 "order_id": order_id,
-                "id_qr": self.id_qr,
+                "id_qr": self._id_qr,
                 "cancel_operation_sum": cancel_operation_sum,
                 "operation_currency": "643",
                 "auth_code": auth_code,
-                "tid": self.tid,
+                "tid": self._tid,
                 "operation_type": operation_type.value
             },
-            pkcs12_filename=self.pkcs12_filename,
-            pkcs12_password=self.pkcs12_password
+            pkcs12_filename=self._pkcs12_filename,
+            pkcs12_password=self._pkcs12_password
         )
         logger.info(response.status_code)
         logger.info(response.text)
 
         return response.json()
 
-    def registry(self, start_period: datetime, end_period: datetime):
+    def registry(self, start_period: datetime, end_period: datetime, registry_type: RegistryType = RegistryType.REGISTRY):
         """
         Запрос реестра операций
         """
@@ -229,18 +213,18 @@ class SyncSberQR(object):
                 'Accept': 'application/json',
                 'Authorization': f'Bearer {access_token}',
                 'RqUID': rq_uid,
-                'x-ibm-client-id': self.client_id,
+                'x-ibm-client-id': self._client_id,
             },
             json={
                 "rqUid": rq_uid,
                 "rqTm": f'{datetime.utcnow().isoformat(timespec="seconds")}Z',
-                "idQR": self.id_qr,
+                "idQR": self._id_qr,
                 "startPeriod": f'{start_period.isoformat(timespec="seconds")}Z',
                 "endPeriod": f'{end_period.isoformat(timespec="seconds")}Z',
-                "registryType": "REGISTRY"
+                "registryType": registry_type.value
             },
-            pkcs12_filename=self.pkcs12_filename,
-            pkcs12_password=self.pkcs12_password
+            pkcs12_filename=self._pkcs12_filename,
+            pkcs12_password=self._pkcs12_password
         )
         logger.info(response.status_code)
         logger.info(response.text)
