bfd8916e0459095c6392e10713735eead3ec1d9a
bl4ckm45k <nonpowa@gmail.com>
2024-01-22 12:46:14 +0300

    bump version and add requirements for sync version

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ from setuptools import setup
 if sys.version_info < (3, 7):
     raise RuntimeError('Your Python version {0} is not supported, please install '
                        'Python 3.7+'.format('.'.join(map(str, sys.version_info[:3]))))
-requirements = ["aiohttp>=3.8.4", "certifi>=2023.5.7", "ujson>=5.8.0", "pytz==2022.1", "qrcode[pil]>=7.3.1",
-                "aioredis>=2.0.1"]
+requirements = ["aiohttp>=3.8.4", "certifi>=2023.11.17", "ujson>=5.9.0", "pytz==2022.1", "qrcode[pil]>=7.3.1",
+                "redis>=4.2.0rc1", "urllib3~=2.0.3"]
 
 setup(
     name='SberQR',
-    version='1.0.3',
+    version='2.0.0',
     author='bl4ckm45k',
     author_email='nonpowa@gmail.com',
-    description='Асинхронная библиотека для работы с SberPay QR/Плати QR.',
+    description='Библиотека для работы с SberPay QR/Плати QR.',
     long_description_content_type="text/markdown",
     url="https://github.com/bl4ckm45k/SberQR",
     license="MIT",
@@ -24,6 +24,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Framework :: AsyncIO',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
