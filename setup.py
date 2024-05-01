b184ba1a7f0abf0082cf27786a25d4e16005c783
bl4ckm45k <nonpowa@gmail.com>
2024-05-01 13:53:49 +0300

    bump pytz

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 if sys.version_info < (3, 7):
     raise RuntimeError('Your Python version {0} is not supported, please install '
                        'Python 3.7+'.format('.'.join(map(str, sys.version_info[:3]))))
-requirements = ["aiohttp>=3.8.4", "certifi>=2023.11.17", "ujson>=5.9.0", "pytz==2022.1", "qrcode[pil]>=7.3.1",
+requirements = ["aiohttp>=3.8.4", "certifi>=2023.11.17", "ujson>=5.9.0", "pytz>=2022.1", "qrcode[pil]>=7.3.1",
                 "redis>=4.2.0rc1", "urllib3~=2.0.3"]
 
 setup(
     name='SberQR',
-    version='2.0.1',
+    version='2.0.2',
     author='bl4ckm45k',
     author_email='nonpowa@gmail.com',
     description='Библиотека для работы с SberPay QR/Плати QR.',
